6a705f3a538ec2b663c7cc52a5241d602f357124
justinbomber <w0913706494@gmail.com>
2023-01-03 16:06:05 +0800

    this can grab the data from coinbase.

--- coibase_data.py
+++ coibase_data.py
@@ -2,21 +2,19 @@ import requests
 from tqdm import tqdm
 import pandas as pd
 from datetime import datetime, timedelta
-import time # 引入time
-timeString = "2022-10-25 00:00:00" # 時間格式為字串 從UTC+0的這個時間開始
+timeString = "2022-10-25 00:00:00" # Time type is String, Begins at UTC+0.
 timeString = datetime.strptime(timeString, '%Y-%m-%d %H:%M:%S')
 
-
-def get_data(timeframe, coin, deltaday, step):
+def get_data(timeframe, coin, deltaday, step, UTC_plus):
     apiUrl = 'https://api.pro.coinbase.com'
     sym = coin #'BTC-USD'
-    barSize = timeframe #秒
+    barSize = timeframe #sec
     timeEnd = timeString - timedelta(days=deltaday) - timedelta(seconds=1)
     timeStart = timeEnd - timedelta(days=step) + timedelta(seconds=1)
     timeStart = timeStart.isoformat()
     timeEnd = timeEnd.isoformat()
     # ------------------------ #
-    # 獲取網頁返回的json
+    # get the json file from web
     parameters = {
         'start':timeStart,
         'end':timeEnd,
@@ -26,19 +24,25 @@ def get_data(timeframe, coin, deltaday, step):
                         headers={'content-type':'application/json'})
     # ------------------------- #
     df = pd.DataFrame(data.json(),columns=['Time','low','high','open','close','volume'])
-    df['Time'] = pd.to_datetime(df['Time'], unit='s') + timedelta(hours=8) # 時間改爲UTC+8
+    df['Time'] = pd.to_datetime(df['Time'], unit='s') + timedelta(hours=UTC_plus) # change the timeframe to UTC+8
     return df
 
 
-def spyder_data(days, timeframe, coin): # 函數引用用這個就好了 上面的不要用
-    # 參數為：總共取幾天(int),時間-秒(int),幣種-{BTC-USD}(str)
+def spyder_data(days, timeframe, coin, UTC_plus): # Use this func to grab the data from coinbase, default.
+    # params:
+    # days: How many days do u need to grab, 300 days is the maximum, time:(int).
+    # timeframe: Which timeframe do you need, !!THE UNIT IS SECOND(S), BE AWARE!!, type:(int).
+    # timeframe: Only available in 15m(900), 2h(7200), 4h(14400)
+    # coin: Which pair, ex: BTC-USD, type:str.
+    # UTC_plus: Which time zone you're in, UTC+2 ? UTC+3 ? type:(int) ex:8,-5.
     btc_list = []
     step = 300/(86400/timeframe)
     timeframe = str(timeframe)
     for i in tqdm(range(0, days, step)):
-        btc_df = get_data(timeframe, coin, i, step)
+        btc_df = get_data(timeframe, coin, i, step, UTC_plus)
         btc_list.append(btc_df)
     pd.concat(btc_list)
     result = pd.concat(btc_list)
-    return result # 數據類型為dataframe
-    # 保存公式為 Dataframe.to_csv('C:/..')
+    return result # the output is DataFrame
+    # Use function: Dataframe.to_csv('C:/..') to save your file by importing pandas.
+
